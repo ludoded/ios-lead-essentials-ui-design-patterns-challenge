ab2a495df40fb7570bff587b97991b48c5825671
Haik Ampardjian <haik.ampardjian@gmail.com>
2021-08-21 22:47:53 +0300

    Fix the ideations and formatting of the feed loading error localized static variable

--- MVVM Module/Source/Feed Presentation/Localized.swift
+++ MVVM Module/Source/Feed Presentation/Localized.swift
@@ -21,12 +21,13 @@ extension Localized {
 				bundle: bundle,
 				comment: "Title for the feed view")
 		}
-
+		
 		static var feedLoadingError: String {
-			NSLocalizedString("FEED_VIEW_CONNECTION_ERROR",
-			                  tableName: table,
-			                  bundle: bundle,
-			                  comment: "Error message on feed loading failure")
+			NSLocalizedString(
+				"FEED_VIEW_CONNECTION_ERROR",
+				tableName: table,
+				bundle: bundle,
+				comment: "Error message on feed loading failure")
 		}
 	}
 }
